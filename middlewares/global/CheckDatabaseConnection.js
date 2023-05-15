be24d6657c61bee0cdb29c27ab18c1e22ff6cc21
Qamar Ali <iamqamarali1@gmail.com>
2023-05-16 00:12:31 +0500

    fixed checkdatabaseconnecton middleware

--- middlewares/global/CheckDatabaseConnection.js
+++ middlewares/global/CheckDatabaseConnection.js
@@ -2,9 +2,15 @@ const InternalServerError = require('../../Errors/InternalServerError')
 const mongoose = require('mongoose')
 
 module.exports = (req, res, next) => {
-    if(mongoose.connection.readyState == 1){
+    if(mongoose.connection.readyState >= 1){
         next()
     }else{
-        next(new InternalServerError("Database connection failed"))
+        setTimeout(() => {
+            if(mongoose.connection.readyState >= 1){
+                next()
+            }else{
+                next(new InternalServerError("Database connection failed"))
+            }
+        }, 8000)
     }
 }
